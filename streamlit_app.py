a02e5238a96b2e01ec32e7b8bcf43b0057ebfa92
varomusic <123842722+varomusic@users.noreply.github.com>
2025-03-19 15:33:52 +0000

    PROYECTO DE GRADO

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Desarrollo de un sistema de monitoreo de la calida del aire basado en IoT Internet de las Cosas y Machine Learning ML para analizar CO en un punto geográfico de la ciudad de Bogotá")
 st.write(
-    "El proyecto está basado en IoT y ML mezclando electronica, mocrocontroladores, creacion de modelos para realizar predicciones de monoxido de carbono."
+    "PROYECTO DE GRADO MAESTRIA 2025 ALVARO ARIZA"
 )
